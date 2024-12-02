ec4d1ace15098237e03f8b4b796b91f393ef6e66
Quinten <quinblom321@gmail.com>
2024-12-02 14:46:39 +0100

    Layout: Header + Footer

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,15 +5,29 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link href="{{ asset('/css/main.css') }}" rel="stylesheet">
+    <title>Open Hiring</title>
 </head>
 <body>
-    <header>
-
+    <header id="main-header">
+        <button id="hamburger-menu-button" type="button">☰</button>
+        <img id="header-logo" src="{{ asset('/images/logos/logo.png') }}" alt="website logo">
+        <img id="profile-picture" src="{{ asset('/images/pfp.png') }}" alt="profile picture">
     </header>
     {{ $slot }}
-    <footer>
 
+    <footer id="main-footer">
+        <div>
+            <ul>
+                <li>☎: (+31)06-12345678</li>
+                <li>✉: Openhiring@oh.nl</li>
+                <li>📍: Pernisstraat 69</li>
+                <li>@: Onze Contact Pagina</li>
+            </ul>
+        </div>
+        <div>
+            <img id="header-logo" src="{{ asset('/images/logos/logo.png') }}" alt="website logo">
+        </div>
     </footer>
 </body>
 </html>
