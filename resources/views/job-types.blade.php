dd3f81bf0d5bcafdb76289208a5bdef6e344cfc2
Kasperdejong <kasperdejong@gmail.com>
2024-12-02 14:18:20 +0100

    De Job-types aangemaakt en een controller ervoor. Plus wat placeholders, zodat ik aan het werk kan

---
+++ resources/views/job-types.blade.php
@@ -0,0 +1,21 @@
+<x-layout>
+
+
+    <h1>Job types</h1>
+
+    <section class="category-blocks">
+        <div>
+            <p>Placeholder blok 1</p>
+        </div>
+
+        <div>
+            <p>Placeholder blok 2</p>
+        </div>
+
+        <div>
+            <p>Placeholder blok 3</p>
+        </div>
+    </section>
+
+
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\JobTypesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
+
+Route::resource('/job-types', JobTypesController::class);
+
 
