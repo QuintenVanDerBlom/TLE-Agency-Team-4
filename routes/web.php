fffe77e0f2aec218b3530a8cd4e7f842f7e6b9c0
crazyclownytr <yashtr02@gmail.com>
2024-12-02 15:38:29 +0100

    user story 4 basics gelegd

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\JobListingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/job-listings', function () {
+    return view('job-listings');
+});
+
+
+Route::get('job-listings/{role}', [JobListingsController::class, 'show']);
