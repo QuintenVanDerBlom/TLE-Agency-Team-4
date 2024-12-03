c04a439b4c77c691a4c7fd1b40610db652800596
ehezo <1068035@hr.nl>
2024-12-03 11:34:32 +0100

    controllers aangemaakt

--- app/Models/JobListingsCategory.php
+++ app/Models/JobListingsCategory.php
@@ -1,20 +1,30 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class JobListingCategory extends Model
+class JobListingsCategory extends Model
 {
     protected $fillable = ['name', 'information'];
 
+    /**
+     * Haal alle categorieën op die gekoppeld zijn aan deze job listing category.
+     */
     public function categories(): BelongsToMany
     {
-        return $this->belongsToMany(Category::class, 'category_job_listing_categories', 'job_listing_category_id', 'category_id');
+        return $this->belongsToMany(
+            Category::class,
+            'category_job_listing_categories',
+            'job_listing_category_id',
+            'category_id'
+        );
     }
 
+    /**
+     * Haal alle job listings op die gekoppeld zijn aan deze job listing category.
+     */
     public function jobListings(): HasMany
     {
         return $this->hasMany(JobListing::class, 'job_listing_category_id');
