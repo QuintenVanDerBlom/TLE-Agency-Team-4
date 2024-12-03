ea796cd953663c3528bd7a9c8c34d2716b838213
CrazyClownytr <145675709+CrazyClownytr@users.noreply.github.com>
2024-12-03 11:06:09 +0100

    Revert "Feature/story 4"

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-use App\Http\Controllers\JobListingsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-
-Route::get('/job-listings', function () {
-    return view('job-listings');
-});
-
-
-Route::get('job-listings/{role}', [JobListingsController::class, 'show']);
