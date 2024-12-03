2d33aa84590215f1b21e1117ec70b8571cc8b515
ehezo <1068035@hr.nl>
2024-12-03 15:39:01 +0100

    bevestigings pagina werkend

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,13 +18,11 @@
     <footer>
 
     <footer id="main-footer">
-        <div>
-            <ul>
-                <li>☎: (+31)06-12345678</li>
-                <li>✉: Openhiring@oh.nl</li>
-                <li>📍: Pernisstraat 69</li>
-                <li>@: Onze Contact Pagina</li>
-            </ul>
+        <div class = "footer-box">
+                <p>☎: (+31)06-12345678</p>
+                <p>✉: Openhiring@oh.nl</p>
+                <p>📍: Pernisstraat 69</p>
+                <p>@: Onze Contact Pagina</p>
         </div>
         <div>
             <img id="header-logo" src="{{ asset('/images/logos/logo.png') }}" alt="website logo">
