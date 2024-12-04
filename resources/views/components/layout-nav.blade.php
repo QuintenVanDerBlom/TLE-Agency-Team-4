1c88371753af3e088b37b99a8085b44595c5345b
ehezo <1068035@hr.nl>
2024-12-04 11:09:21 +0100

    bevestigings pagina werkend

--- resources/views/components/layout-nav.blade.php
+++ resources/views/components/layout-nav.blade.php
@@ -1,10 +1,10 @@
 
 <nav class = "sideNav" id = "sideNav">
     <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-    <a href = "#">Home</a>
-    <a href = "#">Vacatures</a>
-    <a href = "#">Mijn inschrijvingen</a>
-    <a href = "#">Contact</a>
+    <a href = "#" class = "navText">Home</a>
+    <a href = "#" class = "navText">Vacatures</a>
+    <a href = "#" class = "navText">Mijn inschrijvingen</a>
+    <a href = "#" class = "navText">Contact</a>
 </nav>
 <span onclick="openNav()">{{$slot}}</span>
 <script>
