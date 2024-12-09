0c62379c9329b3e8d5f211076901054882c2a4bf
Quinten <quinblom321@gmail.com>
2024-12-09 15:33:21 +0100

    Hold

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,18 +2,20 @@
     <img id="banner-image" src="{{ asset("/images/banner.png") }}" alt="banner image">
 
     <section id="lp-maincontent">
-        <h1>Werk voor iedereen!</h1>
-        <div class="lp-item">
-            <p class="circle-text">Direct reageren. Zonder sollicitatiegesprek, vragen of (voor)oordelen. Een eerlijke kans.</p>
-        </div>
-        <div class="lp-item right-align">
-            <p class="circle-text">Jij bepaalt of je het kunt</p>
-        </div>
-        <div class="lp-item">
-            <p class="circle-text">Snel aan de slag. Met een normaal contract, vanaf dag 1 betaald.</p>
-        </div>
         <div id="lp-button-div">
             <a href="{{ route('categories.index') }}" id="lp-button">Bekijk Vacatures</a>
+        </div>
+        <h1>Werk voor iedereen!</h1>
+        <div class="lp-container">
+            <div class="lp-item">
+                <p class="circle-text">Direct reageren. Zonder sollicitatiegesprek, vragen of (voor)oordelen. Een eerlijke kans.</p>
+            </div>
+            <div class="lp-item right-align">
+                <p class="circle-text">Jij bepaalt of je het kunt</p>
+            </div>
+            <div class="lp-item">
+                <p class="circle-text">Snel aan de slag. Met een normaal contract, vanaf dag 1 betaald.</p>
+            </div>
         </div>
     </section>
 </x-layout>
