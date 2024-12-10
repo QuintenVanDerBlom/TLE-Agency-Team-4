ccddd13b6e1f389acfb2664ae2176aaed0a091e2
ehezo <1068035@hr.nl>
2024-12-10 11:36:08 +0100

    link met de filter pagina

--- routes/web.php
+++ routes/web.php
@@ -36,22 +36,4 @@ Route::get('/joblistings/{id}', [JobListingController::class, 'show'])->name('jo
 
 
 
-// Route om de formulierpagina te tonen
-//Route::get('/aanvullende-informatie', function () {
-//    return view('aanvullende-informatie'); // Zorg ervoor dat het bestand 'aanvullende-informatie.blade.php' heet.
-//})->name('aanvullende-informatie');
-//
-//// Route om het formulier te verwerken
-//Route::post('/aanvullende-informatie', function (\Illuminate\Http\Request $request) {
-//    // Haal de geselecteerde opties op
-//    $keuzes = $request->input('keuze', []); // Geeft een lege array terug als niets geselecteerd is
-//
-//    // Verwerk of sla de gegevens op
-//    // Bijvoorbeeld: sla ze op in een database
-//    // \App\Models\UserData::create(['beperkingen' => json_encode($keuzes)]);
-//
-//    return redirect()->back()->with('success', 'Je keuzes zijn succesvol opgeslagen!');
-//})->name('aanvullende-informatie-verwerken');
 
-
-//Route::post('/filter-vacatures', [JobController::class, 'filter'])->name('filter-vacatures');
